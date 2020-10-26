522096cdbb07151015d7fb2fb15148d8ad8eaf04
Chris Sanders <astericky@gmail.com>
2020-10-26 10:49:59 -0700

    Initial Commit

---
+++ RouteMapTest/RouteMapTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  RouteMapTestApp.swift
+//  RouteMapTest
+//
+//  Created by Chris Sanders on 10/26/20.
+//
+
+import SwiftUI
+
+@main
+struct RouteMapTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
